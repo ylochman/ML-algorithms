fc946e30043c827dc051cfdae65e15a37be5addb
ylochman <lochman.y@gmail.com>
2019-04-24 17:30:34 +0200

    add Yara's sandbox notebook

--- kits19/src/vis_utils.py
+++ kits19/src/vis_utils.py
@@ -10,7 +10,7 @@ def show_slices(overlayed_volume, columns=3, figsize=(50, 50)):
         slice = overlayed_volume[i, :, :, :]
         axes[row][column].imshow(slice)
 
-def multi_slice_viewer(volume):
+def multi_slice_viewer(volume, first_index=0):
     """ Function to display image slices with ability to scroll them accross the depth
         > press K to view next slice
         > press J to view previous slice
@@ -18,8 +18,9 @@ def multi_slice_viewer(volume):
     remove_keymap_conflicts({'j', 'k'})
     fig, ax = plt.subplots()
     ax.volume = volume
-    ax.index = volume.shape[0] // 2
+    ax.index = first_index#volume.shape[0] // 2
     ax.imshow(volume[ax.index])
+    ax.set_title('slice {}'.format(ax.index))
     fig.canvas.mpl_connect('key_press_event', process_key)
         
 def remove_keymap_conflicts(new_keys_set):
@@ -43,8 +44,10 @@ def previous_slice(ax):
     volume = ax.volume
     ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
     ax.images[0].set_array(volume[ax.index])
+    ax.set_title('slice {}'.format(ax.index))
 
 def next_slice(ax):
     volume = ax.volume
     ax.index = (ax.index + 1) % volume.shape[0]
     ax.images[0].set_array(volume[ax.index])
+    ax.set_title('slice {}'.format(ax.index))
